de64b3f9c86bbc66a28b7580996021ec52fbdcc5
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-09-18 12:00:32 -0600

    Create small Destinations dataset to be exported into tests

---
+++ data/destinations.js
@@ -0,0 +1,65 @@
+const allDestinations = [
+  {
+    "id": 1,
+    "destination": "Lima, Peru",
+    "estimatedLodgingCostPerDay": 70,
+    "estimatedFlightCostPerPerson": 400,
+    "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+    "alt": "overview of city buildings with a clear sky"
+  },
+  {
+    "id": 2,
+    "destination": "Stockholm, Sweden",
+    "estimatedLodgingCostPerDay": 100,
+    "estimatedFlightCostPerPerson": 780,
+    "image": "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "city with boats on the water during the day time"
+  },
+  {
+    "id": 3,
+    "destination": "Sydney, Austrailia",
+    "estimatedLodgingCostPerDay": 130,
+    "estimatedFlightCostPerPerson": 950,
+    "image": "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "opera house and city buildings on the water with boats"
+  }, 
+  {
+    "id":10,
+    "destination":"Toronto, Canada",
+    "estimatedLodgingCostPerDay":90,
+    "estimatedFlightCostPerPerson":450,
+    "image":"https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80"
+  },
+  {
+    "id":6,"destination":"Jakarta, Indonesia",
+    "estimatedLodgingCostPerDay":70,
+    "estimatedFlightCostPerPerson":890,
+    "image":"https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt":"lit up city at night"
+  },
+  {
+    "id":7,
+    "destination":"Paris, France",
+    "estimatedLodgingCostPerDay":100,
+    "estimatedFlightCostPerPerson":395,
+    "image":"https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+    "alt":"city during the day time with eiffel tower"
+  },
+  {
+    "id":43,"destination":"Nassau, The Bahamas",
+    "estimatedLodgingCostPerDay":550,
+    "estimatedFlightCostPerPerson":90,
+    "image":"https://images.unsplash.com/photo-1548574505-5e239809ee19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1664&q=80",
+    "alt":"aerial photography of white and blue cruise ships during daytime"
+  },
+  {
+    "id":20,
+    "destination":"Miami, Florida",
+    "estimatedLodgingCostPerDay":158,
+    "estimatedFlightCostPerPerson":275,
+    "image":"https://images.unsplash.com/photo-1514214246283-d427a95c5d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1573&q=80",
+    "alt":"sand with palm trees and tall buildings in the background"
+  }
+];
+
+export default allDestinations;
