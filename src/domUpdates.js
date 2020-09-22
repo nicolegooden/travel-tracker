3e86df5479eb8097ce54a9177c10eb43e4e3835c
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-09-21 20:59:12 -0600

    Create showTripHistory() to fill trip history box with traveler's data

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -3,6 +3,10 @@ import './scripts.js';
 const loginDisplay = document.querySelector('.log-in-display');
 const dashboardDisplay = document.querySelector('.dashboard-display');
 let welcomeText = document.querySelector('.welcome-name');
+let presentTrip = document.querySelector('.present-trip-data');
+let pastTrips = document.querySelector('.past-trips-data');
+let upcomingTrips = document.querySelector('.upcoming-trips-data');
+let pendingTrips = document.querySelector('.pending-trips-data');
 
 let domUpdates = {
   goToMyDashboard() {
@@ -12,6 +16,23 @@ let domUpdates = {
 
   welcomeTravelerByName(firstName) {
     welcomeText.innerText = `Welcome, ${firstName}`;
+  }, 
+
+  showTripHistory(currentTraveler) {
+    if (currentTraveler.presentTrip !== undefined) {
+      presentTrip.innerText = `${currentTraveler.presentTrip}`;
+    } else {
+      presentTrip.innerText = 'You\'re home, bummer!';
+    }
+    currentTraveler.pastTrips.forEach(pastTrip => {
+      pastTrips.innerText += `${pastTrip.myDestinationData.destination} \n `;
+    })
+    currentTraveler.upcomingTrips.forEach(upcomTrip => {
+      upcomingTrips.innerText += `${upcomTrip.myDestinationData.destination} \n `;
+    })
+    currentTraveler.pendingTrips.forEach(pendingTrip => {
+      pendingTrips.innerText += `${pendingTrip.myDestinationData.destination} \n`;
+    })
   }
 }
 
