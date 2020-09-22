7aa3c93766be133554c7a5f298a00fc452d5c8f3
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-09-21 20:12:26 -0600

    Add welcomeTravelerByName() for update in header

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,10 +1,17 @@
+import './scripts.js';
+
 const loginDisplay = document.querySelector('.log-in-display');
 const dashboardDisplay = document.querySelector('.dashboard-display');
+let welcomeText = document.querySelector('.welcome-name');
 
 let domUpdates = {
   goToMyDashboard() {
     loginDisplay.classList.add('hidden-important');
     dashboardDisplay.classList.remove('hidden-important');
+  },
+
+  welcomeTravelerByName(firstName) {
+    welcomeText.innerText = `Welcome, ${firstName}`;
   }
 }
 
